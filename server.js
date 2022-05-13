9681f02693aba5dfe5f4f1c9cd1406dd910900ab
Tan Song Ming <songming@sg.local>
2022-05-13 22:49:24 +0800

    update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/angular-template'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/index.html'));
+        '/dist/angular-template/index.html'));
 });
 app.listen(process.env.PORT || 8080);
