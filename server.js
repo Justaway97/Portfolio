78d115bb603eb7546361983666744e4902f70aaa
Tan Song Ming <songming@sg.local>
2022-05-13 22:21:13 +0800

    update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/songming-portfolio'));
+app.use(express.static(__dirname + '/dist/Portfolio'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/songming-portfolio/index.html'));
+        '/dist/Portfolio/index.html'));
 });
 app.listen(process.env.PORT || 8080);
